fe1e51541656eac7227d54cf725b6c5758322dc8
Shivam jindal <jindalshivam65@gmail.com>
2016-04-23 00:03:47 +0530

    admin view update

--- nsestock/admin.py
+++ nsestock/admin.py
@@ -3,7 +3,10 @@ from .models import stock,userstock
 from django.contrib.auth.models import User
 # Register your models here.
 
+class stockAdmin(admin.ModelAdmin):
+	list_display = ['name','code','price','max_price_of_day','update']
+	list_editable = ['name','code','price','max_price_of_day','update']
 
 
-admin.site.register(stock)
+admin.site.register(stock,stockAdmin)
 admin.site.register(userstock)
