c146396616e744728b3613f354592be9d4d5811e
Shivam jindal <jindalshivam65@gmail.com>
2016-04-28 23:21:47 +0530

    index.html update

--- nsestock/views.py
+++ nsestock/views.py
@@ -4,6 +4,7 @@ from .models import stock,userstock
 from django.views.generic import View
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+from django.template import Context, Template
 import json
 # Create your views here.
 
@@ -20,7 +21,13 @@ class leaderboard(View):
 class index(View):
 	template = 'index.html'
 	def get(self,request):
-		return render(request,self.template, {})
+		stocks = stock.objects.all()
+		shares = []
+		count = 1
+		for share in stocks:
+			shares += [{"name":share.name, "price":share.price, "max":share.max_price_of_day, "code":share.code}]
+			count += 1
+		return render(request,self.template,{'shares':shares})
 	def post(self,request):
 		if request.POST.get('action') == "update":
 			stocks = stock.objects.all()
@@ -33,34 +40,39 @@ class index(View):
 				json.dumps(shares),
 				content_type = 'application/json'
 			)
-		elif request.POST.get('action') = "buy":
+		elif request.POST.get('action') == "buy":
 			try :
 				share = stock.objects.get(code = request.POST.get('code'))
 				quant = request.POST.get('quant')
 				user = None
 				if request.user.is_authenticated():
-        			user = userstocks.objects.get(name = request.user.username)
-        		jsonDec = json.decoder.JSONDecoder()
-        		user_shares = jsonDec.decode(share.shares)
-        		if user.balance >= (quant*share.price):
-        			user.balance -= (quant*share.price)
-        			try :
-        				user_shares[share.code] += quant
-        			except :
-        				user_shares[share.code] = quant
-        			user.shares = json.dumps(user_shares)
-        			user.save()
-        			return HttpResponse(
-        				json.dumps('shares are added in your account'),
-        				content_type = 'application/json'
-        			)
-        		else :
-        			return HttpResponse(
-        				json.dumps("you don't have sufficient balance" ),
-        				content_type = 'application/json'
-        			)
-        else :
-        	return render(request,self.template, {})
+					user = userstocks.objects.get(name = request.user.username)
+				jsonDec = json.decoder.JSONDecoder()
+				user_shares = jsonDec.decode(share.shares)
+				if user.balance >= (quant*share.price):
+					user.balance -= (quant*share.price)
+					try :
+						user_shares[share.code] += quant
+					except :
+						user_shares[share.code] = quant
+					user.shares = json.dumps(user_shares)
+					user.save()
+					return HttpResponse(
+					json.dumps('shares are added in your account'),
+					content_type = 'application/json'
+					)
+				else :
+					return HttpResponse(
+					json.dumps("you don't have sufficient balance" ),
+					content_type = 'application/json'
+					)
+			except :
+				return HttpResponse(
+					json.dumps("operation failed"),
+					content_type = "application/json"
+				)
+		else :
+			return render(request,self.template, {})
 
 
 
