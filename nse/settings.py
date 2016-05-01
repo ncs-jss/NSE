84a79cc22c56537578170bd83f908bad10664ada
Shivam jindal <jindalshivam65@gmail.com>
2016-05-01 09:29:23 +0530

    import/export in Admin added

--- nse/settings.py
+++ nse/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'nsestock', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
+    'nsestock',
 )
 
 MIDDLEWARE_CLASSES = (
--- nsestock/admin.py
+++ nsestock/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from .models import stock,userstock
 from django.contrib.auth.models import User
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-class stockAdmin(admin.ModelAdmin):
-	list_display = ['name','code','price','max_price_of_day','update', 'stock_Exchange']
+class stockResource(resources.ModelResource):
 
+	class Meta:
+		model = stock
+
+class stockAdmin(ImportExportModelAdmin):
+	list_display = ['name','code','price','max_price_of_day','update', 'stock_Exchange']
+	search_fields = ('code',)
+	resource_class = stockResource
+
+class userStockResource(resources.ModelResource):
+	class Meta:
+		model  = userstock
+
+class userStockAdmin(ImportExportModelAdmin):
+	list_display = ['name','shares','balance']
+	search_fields = ('name',)
+	resource_class = userStockResource
 
 admin.site.register(stock,stockAdmin)
-admin.site.register(userstock)
+admin.site.register(userstock,userStockAdmin)
